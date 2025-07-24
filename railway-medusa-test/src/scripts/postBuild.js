b64dec622f4bbf243277052c7984b05c20d10819
camila santacruz <milalovechaton@gmail.com>
2025-07-24 19:27:04 -0300

    replase .medusa/server por .medusa/client

--- railway-medusa-test/src/scripts/postBuild.js
+++ railway-medusa-test/src/scripts/postBuild.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const { execSync } = require('child_process');
 const path = require('path');
 
-const MEDUSA_SERVER_PATH = path.join(process.cwd(), '.medusa', 'server');
+const MEDUSA_SERVER_PATH = path.join(process.cwd(), '.medusa', 'client');
 
 // Check if .medusa/server exists - if not, build process failed
 if (!fs.existsSync(MEDUSA_SERVER_PATH)) {
