da846b26e36c587ea05a4006d1553e371293b46f
camila santacruz <milalovechaton@gmail.com>
2025-07-27 14:27:19 -0300

    cambie dockerfile port 9000/8080

--- railway-medusa-test/medusa-config.ts
+++ railway-medusa-test/medusa-config.ts
@@ -65,7 +65,7 @@ const medusaConfig = {
     }
   },
   admin: {
-    backendUrl: BACKEND_URL,
+    backendUrl: 'https://medusa-railway-production-e40c.up.railway.app',
     disable: SHOULD_DISABLE_ADMIN,
   },
  /* modules: [
